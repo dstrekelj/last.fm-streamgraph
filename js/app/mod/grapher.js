a0368a11c8d689bd9485cbe467c0d938e79fe2b0
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2015-06-16 22:10:40 +0200

    Updated to new version which uses RequireJS.

---
+++ js/app/mod/grapher.js
@@ -0,0 +1,180 @@
+define(['d3', 'app/mod/data', 'app/mod/lfm'], function(d3, Data, LFM) {
+  
+  function draw() {
+    var dataset = Data.filterByPlays(Data.data, Data.minimumPlays);
+    
+    var numberOfDays = dataset[0].value.length,
+        widthOfDay = 40,
+        maxY = Data.findMaxY(dataset),
+        minX = dataset[0].value[0].x,
+        maxX = dataset[0].value[numberOfDays - 1].x;
+    
+    if (!d3.select('#streamgraph').empty()) d3.select('#streamgraph').remove();
+    
+    var axisHeight = 20,
+        dataWidth = numberOfDays * widthOfDay,
+        height = parseInt(d3.select('#graph').style('height')),
+        sliderHeight = 20,
+        statsHeight = 20,
+        width = document.body.clientWidth;
+    
+    var svg = d3.select('#graph')
+      .append('svg')
+      .attr({
+        height : height,
+        id : 'streamgraph',
+        width : '100%'
+      });
+    
+    var group = svg.append('g'),
+        label = d3.select('#label');
+    
+    var stats = svg.append('g')
+      .append('text')
+      .attr({
+        x : '50%',
+        y : height - sliderHeight - statsHeight / 2,
+        'text-anchor' : 'middle',
+        'alignment-baseline' : 'alphabetic'
+      })
+      .text('unique artists: ' + dataset.length + ' / most tracks in a day: ' + Data.findMaxY(Data.data));
+    
+    var x = d3.time.scale().domain([minX, maxX]).range([0, width]),
+        y = d3.scale.linear().domain([0, maxY]).range([axisHeight, height - sliderHeight - statsHeight]),
+        //c = d3.scale.linear().domain([0, dataset.length - 1]).interpolate(d3.interpolateRgb).range(['#b32024', '#0187c5']);
+        c = d3.scale.category20();
+    
+    // Axis
+    var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient('top')
+      .innerTickSize(-height)
+      .outerTickSize(0)
+      .ticks(d3.time.week)
+      .tickPadding(8)
+      .tickFormat(d3.time.format('%d. %b'));
+    
+    // Stack
+    var stack = d3.layout.stack()
+      .offset('silhouette')
+      .values(function stackValues(d) { return d.value; })
+      .x(function stackX(d, i) { return d.x; })
+      .y(function stackY(d) { return d.y; });
+    
+    // Area
+    var area = d3.svg.area()
+      .interpolate('basis')
+      .x(function areaX(d, i) { return x(d.x); })
+      .y0(function areaY0(d) { return y(d.y0); })
+      .y1(function areaY1(d) { return y(d.y0 + d.y); });
+    
+    group.append('g')
+      .call(xAxis)
+      .attr({
+        class     : 'axis',
+        transform : 'translate(0,' + axisHeight + ')'
+      });
+    
+    group.selectAll('path')
+      .data(stack(dataset))
+      .enter()
+        .append('path')
+        .attr({
+          class : 'stream',
+          d     : function pathData(d) { return area(d.value); }
+        })
+        .style('fill', function pathFill(d, i) { return c(i); })
+        .on('mouseenter', function mouseEnter(d, i) {
+          if (d3.selectAll('.stream.clicked').empty()) {
+            label.text(d.key);
+            d3.select(this).classed('focus', true);
+            d3.selectAll('.stream').classed('blur', function(d, j) { return i != (j + 1); });
+          }
+        })
+        .on('mouseleave', function mouseLeave() {
+          if (d3.selectAll('.stream.clicked').empty()) {
+            label.text('');
+            d3.select(this).classed('focus', false);
+            d3.selectAll('.stream').classed('blur', false);
+          }
+        })
+        .on('click', function click(d, i) {
+          //d3.select(this).classed('clicked', !d3.select(this).classed('clicked'));
+          if (d3.select(this).classed('clicked')) {
+            d3.select(this).classed('clicked', false);
+            d3.selectAll('.stream').classed('blur', false);
+          } else {
+            if (d3.selectAll('.stream.clicked').empty()) {
+              d3.select(this).classed('clicked', true);
+              d3.selectAll('.stream').classed('blur', function(d, j) { return i != j + 1; });
+              d3.selectAll('.stream').classed('focus', function(d, j) { return i == j + 1; });
+            }
+          }
+        });
+    
+    // Slider
+    var sliderHeight  = 20,
+        sliderScale   = d3.time.scale().domain([minX, maxX]).range([0, width]),
+        slider        = svg.append('g');
+    
+    var brush = d3.svg.brush()
+      .x(sliderScale)
+      .extent([minX, maxX])
+      .on('brush', sliderBrush);
+    
+    slider.append('rect')
+      .attr('class', 'slider-container')
+      .attr({
+        x     : 0,
+        y     : height - sliderHeight,
+        width : width,
+        height: sliderHeight
+      });
+    
+    slider.append('g')
+      .attr('class', 'slider-brush')
+      .call(brush)
+      .selectAll('rect')
+      .attr({
+        y     : height - sliderHeight,
+        height: sliderHeight
+      });
+    
+    function sliderBrush() {
+      var min = Math.floor(brush.extent()[0]),
+          max = Math.floor(brush.extent()[1]);
+      
+      x.domain([min, max]);
+      group.select('.axis').call(xAxis);
+      group.selectAll('.stream')
+        .data(stack(dataset))
+        .attr('d', function pathData(d) { return area(d.value); });
+    };
+    
+    d3.select(window).on('resize', function resize() {
+      var newWidth = parseInt(d3.select('#graph').style('width'), 10);
+      
+      sliderScale.range([0, newWidth]);
+      slider.select('.slider-container').attr('width', newWidth);
+      slider.select('.slider-brush').call(brush);
+      
+      x.range([0, newWidth]);
+      group.select('.axis').call(xAxis);
+      group.selectAll('.stream')
+        .data(stack(dataset))
+        .attr('d', function pathData(d) { return area(d.value); });
+    });
+    
+    /*
+    group.on('mousemove', function() {
+      var mouse = d3.mouse(this);
+      label.attr({x: mouse[0] + 20, y: mouse[1] + 20});
+    });
+    */
+  };
+  
+  return {
+    draw : draw
+  };
+  
+});
--- js/main.js
+++ js/main.js
@@ -1,74 +1,29 @@
-// Quick-and-dirty way of populating drop down menus
-(function () {
-  var currentDate = new Date(Date.now()),
-      monthNames  = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
-  function addOption(Parent, Text, Value) {
-    var option = document.createElement('option');
-    var text = document.createTextNode(Text);
-    option.setAttribute('value', Value);
-    // Firefox doesn't do well with `innerText`, so we append a text node
-    //option.innerText = Text;
-    option.appendChild(text);
-    document.getElementById(Parent).appendChild(option);
+require.config({
+  baseURL :   'js',
+  paths : {
+    d3 :      'lib/d3.v3.min',
+    lastfm :  'lib/last.fm'
   }
+});
 
-  for (var year = 2006; year <= currentDate.getFullYear(); year++) {
-    addOption('fromY', year, year);
-    addOption('toY', year, year);
-  }
-
-  for (var month = 0; month < 12; month++) {
-    addOption('fromM', monthNames[month], month);
-    addOption('toM', monthNames[month], month);
-  }
-
-  for (var date = 1; date <= 31; date++) {
-    addOption('fromD', date, date);
-    addOption('toD', date, date);
-  }
-}());
-
-/**
- * Parse form data into inline object.
- * @param	form	HTML form
- * @return	Form data as inline object
- */
-function parseForm (Form) {
-  var formElements  = Form.elements,
-      formData      = {};
-
-  for (var i = 0; i < formElements.length; i++) {
-    var element = formElements[i];
-    if (element.value !== "") {
-      formData[element.name] = element.value;
-    }
-  }
-
-  return formData;
-};
-
-// -- APP -- //
-
-var app = new App();
-
-var loader = new Loader('#loader');
-app.setLoadingIndicator(loader);
-
-function onClick(Event) {
-  Event.preventDefault();
-  var data  = parseForm(document.forms.namedItem('parameters')),
-      from  = new Date(Date.UTC(data.fromY, data.fromM, data.fromD)),
-      to    = new Date(Date.UTC(data.toY, data.toM, data.toD));
+require(['ui/parseForm', 'app/app', 'ui/populateDropdown', 'polyfill'], function(ParseForm, App) {
   
-  var params = {
-    user  : data.user,
-    limit : data.limit,
-    from  : from.getTime() / 1000,
-    to    : to.getTime() / 1000
-  };
-  
-  app.request(params, '#graph');
-}
+  function onClick(Event) {
+    Event.preventDefault();
+    var data =  ParseForm(document.forms.namedItem('parameters')),
+        from =  new Date(Date.UTC(data.fromY, data.fromM, data.fromD)),
+        to =    new Date(Date.UTC(data.toY, data.toM, data.toD));
 
-document.getElementById('parameters').addEventListener('submit', onClick, false);
+    var params = {
+      user :          data.user,
+      minimumPlays :  data.minimumPlays,
+      from :          from.getTime() / 1000,
+      to :            to.getTime() / 1000
+    };
+
+    App.request(params);
+  }
+
+  document.getElementById('parameters').addEventListener('submit', onClick, false);
+  
+});
